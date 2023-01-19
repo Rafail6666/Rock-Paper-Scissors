48d9b8e926dca4762cb84757d544e37f0e5a73f2
Liliya Akhmetshina <wsolara18@gmail.com>
2023-01-19 21:23:29 +0500

    fix js

--- Index.js
+++ Index.js
@@ -1,44 +1,44 @@
-<script type="text/javascript">
-        let user1 = 0;
-        let comp = 0;
-        let computer = Math.random();
-        if (computer <= 0.33){
-            computer = 'камень';
-        }else if (computer <= 0.64){
-            computer = 'ножницы';
+const example = () => {
+    let user1 = 0;
+    let comp = 0;
+    let computer = Math.random();
+    if (computer <= 0.33){
+        computer = 'камень';
+    }else if (computer <= 0.64){
+        computer = 'ножницы';
+    }else {
+        computer = 'бумага';
+    }
+    console.log(computer);
+    while (true){
+        const user = prompt('Введите один из вариантов\n1. камень \n2. ножницы\n3. бумага');
+        const number = parseInt(user);
+        if (user === null){
+            console.log('Пользователь '+ user1 +'\nКомпьютер '+ comp)
+            break;
+        }
+        if(computer === 'камень'){
+            if (user === 3){
+                user1++;
+            continue
+            }
         }else {
-            computer = 'бумага';
+            comp++;
         }
-        console.log(computer);
-        while (true){
-            const user = prompt('Введите один из вариантов\n1. камень \n2. ножницы\n3. бумага');
-            const number = parseInt(user);
-            if (user === null){
-                console.log('Пользователь '+ user1 +'\nКомпьютер '+ comp)
-                break;
+        if(computer === 'ножницы'){
+            if (user === 1){
+                user1++;
+            continue
             }
-            if(computer === 'камень'){
-                if (user === 3){
-                    user1++;
-                continue
-                }
-            }else {
-                comp++;
-            }
-            if(computer === 'ножницы'){
-                if (user === 1){
-                    user1++;
-                continue
-                }
-            }else {
-                comp++;
-            }
-            if(computer === 'бумага'){
-                if (user === 2){
-                    user1++;
-                }
-            }else {
-                comp++;
-            }
+        }else {
+            comp++;
         }
-    </script>
+        if(computer === 'бумага'){
+            if (user === 2){
+                user1++;
+            }
+        }else {
+            comp++;
+        }
+    }
+}
